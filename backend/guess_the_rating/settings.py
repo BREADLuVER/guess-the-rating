90676017902c30c27250f4457d90f7aa7575dd16
BREADLuVER <ry2050@nyu.edu>
2024-09-11 11:00:20 -0400

    game prediction migration, repo restructure

--- backend/guess_the_rating/settings.py
+++ backend/guess_the_rating/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'game_prediction',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
