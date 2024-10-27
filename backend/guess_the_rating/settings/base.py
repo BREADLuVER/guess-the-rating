f1ef84bd9c41fac585ddcd7b5eb5ac57d65153f8
BREADLuVER <ry2050@nyu.edu>
2024-10-26 20:33:18 -0400

    added static file for django admin

--- backend/guess_the_rating/settings/base.py
+++ backend/guess_the_rating/settings/base.py
@@ -189,7 +189,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../staticfiles'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
